d1d14f7cf1afed9c9a56b38d9fd04563435a5955
lidengjin <13488601486@163.com>
2020-09-11 11:20:49 +0800

    experience- Calender 时间的处理

--- development/src/test/java/com/quickly/devploment/leetcode/lru/table/ListNode.java
+++ development/src/test/java/com/quickly/devploment/leetcode/lru/table/ListNode.java
@@ -1,11 +1,16 @@
 package com.quickly.devploment.leetcode.lru.table;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * @Author lidengjin
  * @Date 2020/9/9 8:43 下午 求链表的中间节点  快慢指针 同时执行
  * @Version 1.0
  */
-
+@Slf4j
 public class ListNode {
 	int val;
 	ListNode next;
@@ -34,6 +39,49 @@ public class ListNode {
 		}
 
 		return fast == null ? slow : slow.next;
+	}
+
+
+	public static void main(String[] args) {
+		//		List<String> list = Arrays.asList("1","2","3");
+		//		StringBuffer or = new StringBuffer();
+		// 		for (int i = 0; i < list.size() - 1 ; i++) {
+		//			or.append(list.get(i)+",");
+		//		}
+		// 		or.append(list.get(list.size()-1));
+		//		System.out.println(or.toString());
+		//	}
+
+		int terms = 12;
+
+		Date dateTime = new Date();
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(dateTime);
+		calendar.set(Calendar.DAY_OF_MONTH,28);
+		int date = calendar.get(Calendar.DAY_OF_MONTH);
+		int month = calendar.get(Calendar.MONTH) + 1;
+		int year = calendar.get(Calendar.YEAR);
+		log.info("还款日 {}" , date);
+		log.info("还款月 {}" , month);
+		log.info("还款年 {}" , year);
+
+		calendar.set(Calendar.MONTH, month+terms);
+		date = calendar.get(Calendar.DAY_OF_MONTH);
+		month = calendar.get(Calendar.MONTH);
+		year = calendar.get(Calendar.YEAR);
+		log.info("还款日 {}" , date);
+		log.info("还款月 {}" , month);
+		log.info("还款年 {}" , year);
+
+		Calendar calendar1 = Calendar.getInstance();
+		log.info("year {}",calendar1.get(Calendar.YEAR)+"");
+		log.info("year {}",(calendar1.get(Calendar.MONTH) + 1) + "");
+		log.info("year {}",calendar1.get(Calendar.DAY_OF_MONTH) +1+"");
+
+
+
+
+
 	}
 }
 
